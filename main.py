b25b1b028d162aa30b80224b73955be48ed5322a
xmiinaa <153463908+xmiinaa@users.noreply.github.com>
2024-08-18 17:07:14 +0100

    created basic screen

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-import pygame
+# imports and initialise the pygame library
 
+import pygame
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 600
+
+# set up window
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+
+
+def main():
+   
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
