2c8fd41e3e5d10e7aa36dffedd6a18017d69c59f
xmiinaa <153463908+xmiinaa@users.noreply.github.com>
2024-08-22 17:04:23 +0100

    Started box class to make text boxes on screen

--- main.py
+++ main.py
@@ -3,13 +3,35 @@
 import pygame
 pygame.init()
 
+# dimensions of screen
 WIDTH = 1010
 HEIGHT = 675
+
+# colours
+BOX_OUTLINE = (91,164,211)
+BOX_FILL = (211, 245, 253)
+
+# text font
+OCR = pygame.font.SysFont('Ocr', 35)
 
 # set up window
 SCREEN = pygame.display.set_mode([WIDTH,HEIGHT])
 
 MENU_BG = pygame.transform.scale(pygame.image.load('menu-background.png'), (WIDTH, HEIGHT))
+
+class Box:
+    def __init__(self, x, y, colour, width, height):
+        self.x = x
+        self.y = y
+        self.colour = colour
+        self.width = width
+        self.height = height
+
+    def draw(self, x, y, colour, width, height):
+        pygame.draw.rect(SCREEN, colour, pygame.Rect(x, y, width, height),  2, 2)
+
+
+box1 = Box(10, 10, BOX_OUTLINE, 20, 30)
 
 def main():
     SCREEN.blit(MENU_BG, (0, 0))
@@ -19,6 +41,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        box1.draw(10, 10, BOX_OUTLINE, 20, 30)
 
         pygame.display.flip()
 
