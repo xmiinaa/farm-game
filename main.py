5afede61a90a7d2e6589b0ddb60eacf156b66bdf
xmiinaa <153463908+xmiinaa@users.noreply.github.com>
2024-08-23 11:41:00 +0100

    Applied OCR font to text and set screen caption

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pygame
 pygame.init()
 
 # dimensions of screen
-WIDTH = 1010
-HEIGHT = 675
+WIDTH = 1080
+HEIGHT = 720
 
 # colours
 BOX_OUTLINE = (91,164,211)
@@ -13,10 +13,12 @@ BOX_FILL = (211, 245, 253)
 FONT_COLOUR = (2, 100, 106)
 
 # text font
-OCR = pygame.font.SysFont('Resources\OCR', 35)
+OCR = pygame.font.Font('Resources\OCR.ttf', 48)
 
 # set up window
+CLOCK = pygame.time.Clock()
 SCREEN = pygame.display.set_mode([WIDTH,HEIGHT])
+pygame.display.set_caption('THE Farm Game')
 
 MENU_BG = pygame.transform.scale(pygame.image.load('Resources\Images\menu-background.png'), (WIDTH, HEIGHT))
 
@@ -41,12 +43,14 @@ class TextBox(Box):
        self.fontColour = FONT_COLOUR
 
     def draw(self):
-        pygame.draw.rect(SCREEN, self.colourFill, pygame.Rect(self.x, self.y, self.width, self.height), 0, 3)
-        pygame.draw.rect(SCREEN, self.colourBorder, pygame.Rect(self.x, self.y, self.width, self.height), 2, 3)
-        text = OCR.render(self.content, True, self.fontColour)  
-        SCREEN.blit(text, (100, 100) )   
+        pygame.draw.rect(SCREEN, self.colourFill, pygame.Rect(self.x, self.y, self.width, self.height), 0, 5)
+        pygame.draw.rect(SCREEN, self.colourBorder, pygame.Rect(self.x, self.y, self.width, self.height), 2, 5)
 
-titleBox = TextBox(10, 10, 20, 30, "Farm Game")
+        text = OCR.render(self.content, True, self.fontColour)  
+        SCREEN.blit(text, (self.x, self.y) )   
+
+# creating objects from classes
+titleBox = TextBox(WIDTH / 2 - 225, 100, 450, 70, "THE Farm Game")
 
 
 def mainmenu():
