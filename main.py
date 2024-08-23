09d74e58383d9d4d385a92fdcd66d1e6c18d3d11
xmiinaa <153463908+xmiinaa@users.noreply.github.com>
2024-08-23 11:13:09 +0100

    Started TextBox class and only have font left

--- main.py
+++ main.py
@@ -10,21 +10,22 @@ HEIGHT = 675
 # colours
 BOX_OUTLINE = (91,164,211)
 BOX_FILL = (211, 245, 253)
+FONT_COLOUR = (2, 100, 106)
 
 # text font
-OCR = pygame.font.SysFont('Ocr', 35)
+OCR = pygame.font.SysFont('Resources\OCR', 35)
 
 # set up window
 SCREEN = pygame.display.set_mode([WIDTH,HEIGHT])
 
-MENU_BG = pygame.transform.scale(pygame.image.load('menu-background.png'), (WIDTH, HEIGHT))
+MENU_BG = pygame.transform.scale(pygame.image.load('Resources\Images\menu-background.png'), (WIDTH, HEIGHT))
 
 class Box:
-    def __init__(self, x, y, colourFill, colourBorder, width, height):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
-        self.colourFill = colourFill
-        self.colourBorder = colourBorder
+        self.colourFill = BOX_FILL
+        self.colourBorder = BOX_OUTLINE
         self.width = width
         self.height = height
 
@@ -32,8 +33,20 @@ class Box:
         pygame.draw.rect(SCREEN, self.colourFill, pygame.Rect(self.x, self.y, self.width, self.height), 0, 3)
         pygame.draw.rect(SCREEN, self.colourBorder, pygame.Rect(self.x, self.y, self.width, self.height), 2, 3)
 
+class TextBox(Box):
+    def __init__(self, x, y,  width, height, content):
+       super().__init__(x, y, width, height)
+       self.content = content
+       self.font = OCR
+       self.fontColour = FONT_COLOUR
 
-titleBox = Box(10, 10, BOX_FILL, BOX_OUTLINE, 20, 30)
+    def draw(self):
+        pygame.draw.rect(SCREEN, self.colourFill, pygame.Rect(self.x, self.y, self.width, self.height), 0, 3)
+        pygame.draw.rect(SCREEN, self.colourBorder, pygame.Rect(self.x, self.y, self.width, self.height), 2, 3)
+        text = OCR.render(self.content, True, self.fontColour)  
+        SCREEN.blit(text, (100, 100) )   
+
+titleBox = TextBox(10, 10, 20, 30, "Farm Game")
 
 
 def mainmenu():
