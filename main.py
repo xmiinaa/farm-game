e39bc614299e3022e394b5f46a845d392a8b37f6
xmiinaa <153463908+xmiinaa@users.noreply.github.com>
2024-08-23 13:19:03 +0100

    Made and displayed main menu text boxes (un interactive)

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ BOX_FILL = (211, 245, 253)
 FONT_COLOUR = (2, 100, 106)
 
 # text font
-OCR = pygame.font.Font('Resources\OCR.ttf', 48)
+OCR_TITLE = pygame.font.Font('Resources\OCR.ttf', 48)
+OCR_TEXT = pygame.font.Font('Resources\OCR.ttf', 38)
 
 # set up window
 CLOCK = pygame.time.Clock()
@@ -36,24 +37,30 @@ class Box:
         pygame.draw.rect(SCREEN, self.colourBorder, pygame.Rect(self.x, self.y, self.width, self.height), 2, 3)
 
 class TextBox(Box):
-    def __init__(self, x, y,  width, height, content):
+    def __init__(self, x, y,  width, height, font, content):
        super().__init__(x, y, width, height)
        self.content = content
-       self.font = OCR
+       self.font = font
        self.fontColour = FONT_COLOUR
 
     def draw(self):
         pygame.draw.rect(SCREEN, self.colourFill, pygame.Rect(self.x, self.y, self.width, self.height), 0, 5)
         pygame.draw.rect(SCREEN, self.colourBorder, pygame.Rect(self.x, self.y, self.width, self.height), 2, 5)
 
-        text = OCR.render(self.content, True, self.fontColour)  
-        SCREEN.blit(text, (self.x, self.y) )   
+        text = self.font.render(self.content, True, self.fontColour) 
+        textRect = text.get_rect(center = (self.width / 2 + self.x, self.height / 2 + self.y))
+        SCREEN.blit(text, textRect )   
 
 # creating objects from classes
-titleBox = TextBox(WIDTH / 2 - 225, 100, 450, 70, "THE Farm Game")
+titleBox = TextBox(WIDTH / 2 - 225, 100, 450, 80, OCR_TITLE, "THE Farm Game")
+
+newGameBox = TextBox(WIDTH / 2 - 150, 250, 300, 70, OCR_TEXT, "New Game")
+loadGameBox = TextBox(WIDTH / 2 - 150, 350, 300, 70, OCR_TEXT, "Load Game")
+instructionsBox = TextBox(WIDTH / 2 - 150, 450, 300, 70, OCR_TEXT, "How To Play")
+settingsBox = TextBox(WIDTH / 2 - 150, 550, 300, 70, OCR_TEXT, "Settings")
 
 
-def mainmenu():
+def mainmenu_loop():
     SCREEN.blit(MENU_BG, (0, 0))
     
     for event in pygame.event.get():
@@ -63,10 +70,12 @@ def mainmenu():
         
 
     titleBox.draw()
+    newGameBox.draw()
+    loadGameBox.draw()
+    instructionsBox.draw()
+    settingsBox.draw()
 
     pygame.display.flip()
-
-
 
 
 def main():
@@ -74,7 +83,7 @@ def main():
     running = True
 
     while running:
-        mainmenu()
+        mainmenu_loop()
 
 
 if __name__ == "__main__":
